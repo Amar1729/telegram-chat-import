2268f26b779a458aa46a2f6f55f4e71e8d2c3e6c
Amar1729 <amar.paul16@gmail.com>
2021-02-03 02:01:09 -0500

    add media handling

--- main.py
+++ main.py
@@ -3,6 +3,46 @@
 import datetime
 import json
 import sys
+import urllib.request
+
+
+OLD_NAME = [""]
+
+
+def groupme_media_name(fname) -> str:
+    """ media from groupme urls is formatted oddly, like:
+    828x809.jpeg.b083fc7771d848d78c8466f558202063
+
+        Want to return:
+    b083fc7771d848d78c8466f558202063.828x809.jpeg
+    """
+    res, ext, _id = fname.split(".")
+    ext = "jpg" if ext == "jpeg" else ext
+    return ".".join([_id, res, ext])
+
+
+def download_file(url, fname):
+    with urllib.request.urlopen(url) as _url:
+        with open(fname, "wb") as f:
+            f.write(_url.read())
+
+
+def get_text(msg) -> str:
+    text = msg["text"]
+
+    for attachment in msg["attachments"]:
+        if attachment["type"] in ["image", "video"]:
+            # first download the file
+            dst_file = groupme_media_name(attachment["url"].split("/")[-1])
+            download_file(attachment["url"], dst_file)
+
+            # then update our text
+            if text:
+                text = f"{dst_file} (file attached)" + "\n" + text
+            else:
+                text = f"{dst_file} (file attached)"
+
+    return text
 
 
 def format_msg(msg) -> str:
@@ -12,10 +52,8 @@ def format_msg(msg) -> str:
     if msg["user_id"] == "system":
         return ""
 
-    # add a check for text being empty? what does media look like?
-    # should result in a line that looks like:
-    # 6/21/20, 22:10 - Amar Paul: IMG-20200621-WA0000.jpg (file attached)
-    return f"{d_res} - {msg['name']}: {msg['text']}"
+    text = get_text(msg)
+    return f"{d_res} - {msg['name']}: {text}"
 
 
 def main(fname):
